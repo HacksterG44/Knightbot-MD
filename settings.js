02752b00ec0351a6226ed86f58862c5308887f68
HacksterG44 <hacksterglinx@gmail.com>
2025-04-30 13:01:13 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'Glitchy_pack',
+  author: '‎Dominic',
+  botName: "Glitchy_Bot",
+  botOwner: 'Professor Dominic', // Your name
+  ownerNumber: '260771200934', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
